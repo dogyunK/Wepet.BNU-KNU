cff64e563a41d51b9ef9f3fc74cbe0f1a181dc5f
dogyun-k <61974613+dogyun-k@users.noreply.github.com>
2020-06-07 19:20:36 +0900

    맡기미 데이터베이스 추가

--- userinfo.sql
+++ userinfo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- 생성 시간: 20-06-07 10:00
+-- 생성 시간: 20-06-07 10:19
 -- 서버 버전: 8.0.20
 -- PHP 버전: 7.4.6
 
@@ -47,6 +47,32 @@ CREATE TABLE `돌보미` (
 INSERT INTO `돌보미` (`dolID`, `name`, `password`, `phone`, `date1`, `date2`, `addr`, `addrex`, `dolCondition`, `requierment`) VALUES
 (8, '김철수', '1234', '01012345678', '06/01/2020', '06/07/2020', '서울 송파구 풍성로 4-3', '행복아파트', '얌전했으면 좋겠어요', '');
 
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `맡기미`
+--
+
+CREATE TABLE `맡기미` (
+  `matID` int NOT NULL,
+  `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `phone` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `date1` text COLLATE utf8mb4_unicode_ci,
+  `date2` text COLLATE utf8mb4_unicode_ci,
+  `pet1` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `pet2` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `petnumber1` text COLLATE utf8mb4_unicode_ci,
+  `petnumber2` text COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 테이블의 덤프 데이터 `맡기미`
+--
+
+INSERT INTO `맡기미` (`matID`, `name`, `password`, `phone`, `date1`, `date2`, `pet1`, `pet2`, `petnumber1`, `petnumber2`) VALUES
+(1, 'asdfasdf', 'asdf', '123', '123', '123', '중형견', '중형견', '2', '2');
+
 --
 -- 덤프된 테이블의 인덱스
 --
@@ -58,6 +84,12 @@ ALTER TABLE `돌보미`
   ADD PRIMARY KEY (`dolID`);
 
 --
+-- 테이블의 인덱스 `맡기미`
+--
+ALTER TABLE `맡기미`
+  ADD PRIMARY KEY (`matID`);
+
+--
 -- 덤프된 테이블의 AUTO_INCREMENT
 --
 
@@ -66,6 +98,12 @@ ALTER TABLE `돌보미`
 --
 ALTER TABLE `돌보미`
   MODIFY `dolID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- 테이블의 AUTO_INCREMENT `맡기미`
+--
+ALTER TABLE `맡기미`
+  MODIFY `matID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
